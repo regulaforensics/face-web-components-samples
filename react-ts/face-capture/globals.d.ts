84fd92e2b8a170acb095c1e0377d9508a1bf37bd
Artem Tsybulko <tsybulko.artem@gmail.com>
2023-03-20 17:03:22 +0300

    Update samples

--- react-ts/face-capture/globals.d.ts
+++ react-ts/face-capture/globals.d.ts
@@ -1,22 +1,11 @@
 import { HTMLAttributes, DetailedHTMLProps } from 'react';
-import {
-    FaceLivenessWebComponent,
-    FaceDetectionWebComponent,
-    FullScreenContainer,
-    IFaceLiveness,
-    IFaceDetection,
-    IBaseComponent,
-} from '@regulaforensics/vp-frontend-face-components';
+import { FaceDetectionWebComponent, IFaceDetection } from '@regulaforensics/vp-frontend-face-components';
 
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            'face-liveness': DetailedHTMLProps<IFaceLiveness & HTMLAttributes<FaceLivenessWebComponent>,
-                FaceLivenessWebComponent>;
             'face-capture': DetailedHTMLProps<IFaceDetection & HTMLAttributes<FaceDetectionWebComponent>,
                 FaceDetectionWebComponent>;
-            'fullscreen-container': DetailedHTMLProps<IBaseComponent & HTMLAttributes<FullScreenContainer>,
-                FullScreenContainer>;
         }
     }
 }
